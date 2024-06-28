3164cac611fd64b8c03c033b40b7550c20433cb7
Kunj Patel <kunjd26@gmail.com>
2024-06-28 18:07:00 +0530

    Add dynamic parser's work is almost completed, Add swagger docs details in new APIs endpoint but get fetch error, Still this issue is not solve.

--- app.py
+++ app.py
@@ -7,14 +7,16 @@ from src.download_file import download_file
 from src.process_data import analysis_data
 from src.file_status import get_file_status, update_file_status, get_recent_files
 from src.delete_file import delete_file
+from src.dynamic_parser import get_file_header, dynamic_parse_file
 from flasgger import Swagger
 
 app = Flask(__name__)
 CORS(app)
 swagger = Swagger(app)
 
+
 # Upload endpoint
-@app.route('/api/upload', methods=['POST'])
+@app.route("/api/upload", methods=["POST"])
 def upload_file_endpoint():
     """
     Upload File Endpoint
@@ -34,13 +36,23 @@ def upload_file_endpoint():
     """
     try:
         # Check if the request contains a file.
-        if 'file' not in request.files:
-            return jsonify({"status": "fail", "message": "No file part present in request."}), 400
+        if "file" not in request.files:
+            return (
+                jsonify(
+                    {"status": "fail", "message": "No file part present in request."}
+                ),
+                400,
+            )
 
-        file = request.files['file']
-        if file.filename == '':
-            return jsonify({"status": "fail", "message": "No file is selected for upload."}), 400
-        
+        file = request.files["file"]
+        if file.filename == "":
+            return (
+                jsonify(
+                    {"status": "fail", "message": "No file is selected for upload."}
+                ),
+                400,
+            )
+
         # Upload the file.
         status, result = upload_file(file)
 
@@ -48,7 +60,16 @@ def upload_file_endpoint():
             # Update the file status to 0 (uploaded).
             status1, result1 = update_file_status(result, 0, file.filename)
             if status1 == 0:
-                return jsonify({"status": "success", "message": "File uploaded successfully.", "token": result}), 200
+                return (
+                    jsonify(
+                        {
+                            "status": "success",
+                            "message": "File uploaded successfully.",
+                            "token": result,
+                        }
+                    ),
+                    200,
+                )
             else:
                 return jsonify({"status": "error", "message": result1}), 500
         elif status == 1:
@@ -56,14 +77,17 @@ def upload_file_endpoint():
         elif status == 2:
             return jsonify({"status": "error", "message": result}), 500
         else:
-            return jsonify({"status": "error", "message": "Unknown error occurred."}), 500
+            return (
+                jsonify({"status": "error", "message": "Unknown error occurred."}),
+                500,
+            )
 
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
 
 
 # Execute endpoint
-@app.route('/api/execute/', methods=['GET'])
+@app.route("/api/execute/", methods=["GET"])
 def execute_file_endpoint():
     """
     Execute File Endpoint
@@ -89,20 +113,20 @@ def execute_file_endpoint():
     """
     try:
         # Check if the token is provided.
-        file_token = request.args.get('token')
+        file_token = request.args.get("token")
         if not file_token:
             return jsonify({"status": "fail", "message": "Token not provided."}), 400
-        
+
         # Check if the static database only parameter provided.
-        static_db_only = request.args.get('static_db_only')
+        static_db_only = request.args.get("static_db_only")
         if not static_db_only:
             static_db_only = False
         else:
-            if static_db_only.lower() == 'true':
+            if static_db_only.lower() == "true":
                 static_db_only = True
             else:
                 static_db_only = False
-        
+
         # Parse file through general parser.
         status, result = parse_file(file_token)
 
@@ -127,7 +151,15 @@ def execute_file_endpoint():
                 # Update the file status to 2 (processed).
                 status1, result1 = update_file_status(file_token, 2)
                 if status1 == 0:
-                    return jsonify({"status": "success", "message": "File executed successfully."}), 200
+                    return (
+                        jsonify(
+                            {
+                                "status": "success",
+                                "message": "File executed successfully.",
+                            }
+                        ),
+                        200,
+                    )
                 else:
                     return jsonify({"status": "error", "message": result1}), 500
             elif status == 1:
@@ -140,14 +172,17 @@ def execute_file_endpoint():
                 else:
                     return jsonify({"status": "error", "message": result1}), 500
             else:
-                return jsonify({"status": "error", "message": "Unknown error occurred."}), 500
+                return (
+                    jsonify({"status": "error", "message": "Unknown error occurred."}),
+                    500,
+                )
 
     except Exception as e:
         return jsonify({"status": "error", "message": str(e)}), 500
 
 
 # Download endpoint
-@app.route('/api/download/', methods=['GET'])
+@app.route("/api/download/", methods=["GET"])
 def download_file_endpoint():
     """
     Download File Endpoint
@@ -168,27 +203,32 @@ def download_file_endpoint():
     """
     try:
         # Check if the token is provided.
-        file_token = request.args.get('token')
+        file_token = request.args.get("token")
         if not file_token:
             return jsonify({"status": "fail", "message": "Token not provided."}), 400
-        
+
         status, result = download_file(file_token)
 
         if status == 0:
-            return send_file(result, as_attachment=True, download_name=f"{file_token}.csv")
+            return send_file(
+                result, as_attachment=True, download_name=f"{file_token}.csv"
+            )
         elif status == 1:
             return jsonify({"status": "fail", "message": result}), 400
         elif status == 2:
             return jsonify({"status": "error", "message": result}), 500
         else:
-            return jsonify({"status": "error", "message": "Unknown error occurred."}), 500
+            return (
+                jsonify({"status": "error", "message": "Unknown error occurred."}),
+                500,
+            )
 
     except Exception as e:
         return jsonify({"error": {"message": str(e)}}), 500
 
 
 # Analysis endpoint
-@app.route('/api/analysis/', methods=['GET'])
+@app.route("/api/analysis/", methods=["GET"])
 def analysis_file_endpoint():
     """
     Analysis File Endpoint
@@ -219,22 +259,22 @@ def analysis_file_endpoint():
     """
     try:
         # Check if the token is provided.
-        file_token = request.args.get('token')
+        file_token = request.args.get("token")
         if not file_token:
             return jsonify({"status": "fail", "message": "Token not provided."}), 400
-        
-        n = request.args.get('n')
+
+        n = request.args.get("n")
         if not n:
             n = 10
         else:
             n = int(n)
 
-        columns = request.args.get('columns')
+        columns = request.args.get("columns")
         if not columns:
             status, result = analysis_data(file_token, n)
         else:
             status, result = analysis_data(file_token, n, columns)
-            
+
         if status == 0:
             return jsonify({"status": "success", "data": result}), 200
         elif status == 1:
@@ -242,14 +282,17 @@ def analysis_file_endpoint():
         elif status == 2:
             return jsonify({"status": "error", "message": result}), 500
         else:
-            return jsonify({"status": "error", "message": "Unknown error occurred."}), 500
-    
+            return (
+                jsonify({"status": "error", "message": "Unknown error occurred."}),
+                500,
+            )
+
     except Exception as e:
         return jsonify({"error": {"message": str(e)}}), 500
 
 
 # Status endpoint
-@app.route('/api/status/', methods=['GET'])
+@app.route("/api/status/", methods=["GET"])
 def status_file_endpoint():
     """
     Status File Endpoint
@@ -270,12 +313,12 @@ def status_file_endpoint():
     """
     try:
         # Check if the token is provided.
-        file_token = request.args.get('token')
+        file_token = request.args.get("token")
         if not file_token:
             return jsonify({"status": "fail", "message": "Token not provided."}), 400
-        
+
         status, result = get_file_status(file_token)
-            
+
         if status == 0:
             return jsonify({"status": "success", "message": result}), 200
         elif status == 1:
@@ -283,14 +326,17 @@ def status_file_endpoint():
         elif status == 2:
             return jsonify({"status": "error", "message": result}), 500
         else:
-            return jsonify({"status": "error", "message": "Unknown error occurred."}), 500
-    
+            return (
+                jsonify({"status": "error", "message": "Unknown error occurred."}),
+                500,
+            )
+
     except Exception as e:
         return jsonify({"error": {"message": str(e)}}), 500
 
 
 # Recent file endpoint
-@app.route('/api/recent-files', methods=['GET'])
+@app.route("/api/recent-files", methods=["GET"])
 def recent_file_endpoint():
     """
     Recent Files Endpoint
@@ -311,14 +357,14 @@ def recent_file_endpoint():
     """
     try:
         # Check if the token is provided.
-        n = request.args.get('n')
+        n = request.args.get("n")
         if not n:
             n = 5
         else:
             n = int(n)
-        
+
         status, result = get_recent_files(n)
-            
+
         if status == 0:
             return jsonify({"status": "success", "data": result}), 200
         elif status == 1:
@@ -326,14 +372,17 @@ def recent_file_endpoint():
         elif status == 2:
             return jsonify({"status": "fail", "message": result}), 500
         else:
-            return jsonify({"status": "error", "message": "Unknown error occurred."}), 500
-    
+            return (
+                jsonify({"status": "error", "message": "Unknown error occurred."}),
+                500,
+            )
+
     except Exception as e:
         return jsonify({"error": {"message": str(e)}}), 500
 
 
 # Delete file endpoint
-@app.route('/api/delete-file', methods=['DELETE'])
+@app.route("/api/delete-file", methods=["DELETE"])
 def delete_file_endpoint():
     """
     Delete File Endpoint
@@ -354,12 +403,12 @@ def delete_file_endpoint():
     """
     try:
         # Check if the token is provided.
-        file_token = request.args.get('token')
+        file_token = request.args.get("token")
         if not file_token:
             return jsonify({"status": "fail", "message": "Token not provided."}), 400
-        
+
         status, result = delete_file(file_token)
-            
+
         if status == 0:
             return jsonify({"status": "success", "message": result}), 200
         elif status == 1:
@@ -367,12 +416,190 @@ def delete_file_endpoint():
         elif status == 2:
             return jsonify({"status": "error", "message": result}), 500
         else:
-            return jsonify({"status": "error", "message": "Unknown error occurred."}), 500
-    
+            return (
+                jsonify({"status": "error", "message": "Unknown error occurred."}),
+                500,
+            )
+
     except Exception as e:
         return jsonify({"error": {"message": str(e)}}), 500
 
 
+# Get header of file
+@app.route("/api/file-header/", methods=["GET"])
+def get_file_header_endpoint():
+    """
+    Get File Header Endpoint
+    ---
+    parameters:
+    - name: token
+        in: query
+        type: string
+        required: true
+        description: The token of the file to retrieve the header from.
+    responses:
+    200:
+        description: Header retrieved successfully.
+    400:
+        description: Token not provided or error occurred while parsing the file.
+    500:
+        description: Error occurred while updating the file status or unknown error occurred.
+    """
+    try:
+        # Check if the token is provided.
+        file_token = request.args.get("token")
+        if not file_token:
+            return jsonify({"status": "fail", "message": "Token not provided."}), 400
+
+        # Parse file through general parser.
+        status, result = get_file_header(file_token)
+
+        if status == 1:
+            return jsonify({"status": "fail", "message": result}), 400
+        elif status == 2:
+            # Update the file status to 3 (error in parsing).
+            status1, result1 = update_file_status(file_token, 3)
+            if status1 == 0:
+                return jsonify({"status": "error", "message": result}), 500
+            else:
+                return jsonify({"status": "error", "message": result1}), 500
+        else:
+            return jsonify({"status": "success", "message": result}), 200
+
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
+
+
+# Execute file with dynamic parser
+@app.route("/api/execute-dynamic/", methods=["POST"])
+def dynamic_execute_file_endpoint():
+    """
+    Execute File with Dynamic Parser Endpoint
+    ---
+    parameters:
+    - name: token
+        in: query
+        type: string
+        required: true
+        description: The token of the file to be executed.
+    - name: static_db_only
+        in: query
+        type: boolean
+        required: false
+        description: Whether to use only the static database or not.
+    - name: column_mapping
+        in: body
+        required: true
+        schema:
+            type: object
+            properties:
+                column_mapping:
+                    type: object
+                    description: The column mapping for the dynamic parser.
+    responses:
+    200:
+        description: File executed successfully.
+    400:
+        description: Token not provided or error occurred while parsing the file.
+    500:
+        description: Error occurred while executing the file.
+    """
+    try:
+        # Check if the request contains a JSON body.
+        if request.content_type != "application/json":
+            return (
+                jsonify(
+                    {
+                        "status": "fail",
+                        "message": "Content-Type must be application/json.",
+                    }
+                ),
+                415,
+            )
+
+        # Check if the token is provided.
+        file_token = request.args.get("token")
+        if not file_token:
+            return jsonify({"status": "fail", "message": "Token not provided."}), 400
+
+        # Check if the static database only parameter provided.
+        static_db_only = request.args.get("static_db_only")
+        if not static_db_only:
+            static_db_only = False
+        else:
+            if static_db_only.lower() == "true":
+                static_db_only = True
+            else:
+                static_db_only = False
+
+        if "column_mapping" not in request.json:
+            return (
+                jsonify(
+                    {
+                        "status": "fail",
+                        "message": "Column mapping must be provided in the request body.",
+                    }
+                ),
+                400,
+            )
+
+        # Parse file through general parser.
+        status, result = dynamic_parse_file(file_token, request.json["column_mapping"])
+
+        if status == 1:
+            return jsonify({"status": "fail", "message": result}), 400
+        elif status == 2:
+            # Update the file status to 3 (error in parsing).
+            status1, result1 = update_file_status(file_token, 3)
+            if status1 == 0:
+                return jsonify({"status": "error", "message": result}), 500
+            else:
+                return jsonify({"status": "error", "message": result1}), 500
+        else:
+            # Update the file status to 1 (processing).
+            status1, result1 = update_file_status(file_token, 1)
+            if status1 == 0:
+                # Append the files.
+                status, result = append_fields(file_token, static_db_only)
+            else:
+                return jsonify({"status": "error", "message": result1}), 500
+            if status == 0:
+                # Update the file status to 2 (processed).
+                status1, result1 = update_file_status(file_token, 2)
+                if status1 == 0:
+                    return (
+                        jsonify(
+                            {
+                                "status": "success",
+                                "message": "File executed successfully.",
+                            }
+                        ),
+                        200,
+                    )
+                else:
+                    return jsonify({"status": "error", "message": result1}), 500
+            elif status == 1:
+                return jsonify({"status": "fail", "message": result}), 400
+            elif status == 2:
+                # Update the file status to 4 (error in processing).
+                status1, result1 = update_file_status(file_token, 4)
+                if status1 == 0:
+                    return jsonify({"status": "error", "message": result}), 500
+                else:
+                    return jsonify({"status": "error", "message": result1}), 500
+            else:
+                return (
+                    jsonify({"status": "error", "message": "Unknown error occurred."}),
+                    500,
+                )
+
+    except Exception as e:
+        return jsonify({"status": "error", "message": str(e)}), 500
+
+
 # Run the Flask app
-if __name__ == '__main__':
+if __name__ == "__main__":
+    import logging
+
+    logging.basicConfig(level=logging.DEBUG)
     app.run(host="0.0.0.0", port=5000)
---
+++ src/dynamic_parser.py
@@ -0,0 +1,147 @@
+import os
+import pandas as pd
+
+# Folder to store uploaded files and generated files
+UPLOAD_FOLDER = os.path.join("files", "uploaded")
+PARSED_FOLDER = os.path.join("files", "parsed")
+
+
+def get_file_header(file_token):
+    try:
+        file_name = f"uploaded-{file_token}.csv"
+        file_path = os.path.join(UPLOAD_FOLDER, file_name)
+
+        if not os.path.isfile(file_path):
+            return 1, "File not found."
+
+        # Process the file
+        df = read_csv_with_header_detection(file_path)
+
+        if len(df.columns) <= 1:
+            return 1, "No columns detected."
+
+        # Strip whitespace from column names
+        df.columns = df.columns.str.strip()
+
+        # Convert column names to lower case for case insensitive comparison
+        df.columns = df.columns.str.lower()
+
+        return 0, df.columns.tolist()
+
+    except Exception as e:
+        return 2, str(e)
+
+
+def dynamic_parse_file(file_token, column_mapping):
+    try:
+        file_name = f"uploaded-{file_token}.csv"
+        file_path = os.path.join(UPLOAD_FOLDER, file_name)
+
+        if not os.path.isfile(file_path):
+            return 1, "File not found."
+
+        # Process the file
+        df = read_csv_with_header_detection(file_path)
+
+        # Strip whitespace from column names
+        df.columns = df.columns.str.strip()
+
+        # Convert column names to lower case for case insensitive comparison
+        df.columns = df.columns.str.lower()
+
+        # Check column mapping dict contain at least following keys destination ip, destination port
+        if not all(
+            col in column_mapping for col in ["destination ip", "destination port"]
+        ):
+            return (
+                1,
+                "Column mapping must contain 'destination ip' and 'destination port' keys.",
+            )
+
+        # Check all keys data type must be list
+        if not all(isinstance(column_mapping[col], list) for col in column_mapping):
+            return 1, "All values in column mapping must be array(list)."
+
+        # Create a dictionary for extracted columns
+        extracted_data = {}
+
+        # Extract the columns based on the mapping
+        for output_col, input_cols in column_mapping.items():
+            col_name = find_column(df, input_cols)
+            if col_name:
+                extracted_data[output_col] = df[col_name]
+            else:
+                # print(f"Warning: None of the possible columns for '{output_col}' were found in the input file.")
+                extracted_data[output_col] = None
+
+        # Create a new DataFrame with the extracted data
+        extracted_df = pd.DataFrame(extracted_data)
+
+        # Create the upload folder if it doesn't exist.
+        os.makedirs(PARSED_FOLDER, exist_ok=True)
+
+        output_csv = f"parsed-{file_token}.csv"
+        output_csv_path = os.path.join(PARSED_FOLDER, output_csv)
+
+        # Write the extracted data to the output CSV file
+        extracted_df.to_csv(output_csv_path, index=False)
+
+        return 0, None
+
+    except Exception as e:
+        return 2, str(e)
+
+
+def read_csv_with_header_detection(file_path):
+    delimiters = [",", ";", "\t", "|"]
+    expected_columns = [
+        "phone number",
+        "roaming circle",
+        "source ip",
+        "source port",
+        "destination ip",
+        "destination port",
+        "imei",
+        "mac id",
+        "imsi",
+        "cell global id",
+        "access point name",
+        "data uplink volume",
+        "data downlink volume",
+        "rat",
+        "pgw/ggsn ip",
+        "billing type",
+        "timestamp",
+        "duration",
+        "ipv6",
+    ]
+
+    for delimiter in delimiters:
+        try:
+            with open(file_path, "r") as file:
+                lines = file.readlines()
+
+            header_row = 0
+            for i, line in enumerate(lines):
+                if any(col in line for col in expected_columns):
+                    header_row = i
+                    break
+
+            df = pd.read_csv(
+                file_path, delimiter=delimiter, skiprows=header_row, low_memory=False
+            )
+            return df
+        except pd.errors.ParserError:
+            continue
+
+    raise pd.errors.ParserError(
+        "Unable to parse the CSV file with the given delimiters."
+    )
+
+
+def find_column(df, possible_columns):
+    lower_case_df_columns = [col.lower() for col in df.columns]
+    for col in possible_columns:
+        stripped_col = col.strip().lower()
+        if stripped_col in lower_case_df_columns:
+            return stripped_col
